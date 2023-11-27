8efa3d7d93ce96c90cf7ce2dca74a28396eacfc8
gamatoza <gamatext@yandex.ru>
2023-11-27 08:07:01 +0300

    before update

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>LoL</title>
+</head>
+    <body>
+        <table>
+            <tr><td>Слово</td><td>Перевод</td></tr>
+            <tr><td>Cabbage</td><td>Капуста</td></tr>
+            <tr><td>Carrot</td><td>Морковь</td></tr>
+            <tr><td>Potato</td><td>Картофель</td></tr>
+            <tr><td>Tomato</td><td>Помидор</td></tr>
+        </table>
+        <script src="node_modules/jquery/dist/jquery.min.js"></script>
+        <script type="text/javascript">
+            jQuery(document).ready(function (){
+                $("#id").css("width","10px");
+            });
+        </script>
+    </body>
+</html>
