7b39122c7b0049ec0d13563e679005df25a713d5
Gamatoza <dev.tihonvlasov@gmail.com>
2023-12-04 17:01:18 +0300

    admin_panel start

--- admin_panel.php
+++ admin_panel.php
@@ -1,6 +1,8 @@
 <?php
+require_once 'config.php';
+global $conn;
 require_once 'session.php';
 
 if(!isset($_SESSION['uid']) or $_SESSION['uid'] == -1)
     header("location: login.php");
-print_r ($_SESSION);
+
--- config.php
+++ config.php
@@ -3,17 +3,18 @@ require_once 'session.php';
 
 $conn = null;
 
-define('USER', 'root');
-define('PASSWORD', '');
-define('HOST', 'localhost');
-define('DATABASE', 'questionnaire');
+$user = 'root';
+$password = '';
+$host = 'localhost';
+$database = 'questionnaire';
+$options = array(
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+);
 try {
-    $conn = new PDO("mysql:host=".HOST.";dbname=".DATABASE, USER, PASSWORD);
+    $conn = new PDO("mysql:host=".$host.";dbname=".$database, $user, $password, $options);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     exit("Error: " . $e->getMessage());
 }
 
-
-
-?>
