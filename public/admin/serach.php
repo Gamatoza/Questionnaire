e2fdaa95dbf5fac593cc5c3bd5f7c909c58311ca
Gamatoza <dev.tihonvlasov@gmail.com>
2023-12-15 15:25:55 +0300

    little lazy changes

--- public/admin/serach.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../../vendor/autoload.php';
-$cfg = AppConfig::getInstance();
-$conn = $cfg->connection;
-
-if(!isset($_SESSION['uid']) or $_SESSION['uid'] == -1)
-    header("location: login.php");
