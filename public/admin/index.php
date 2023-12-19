0cac2ac63ad65406cf5cc28019cbea278b2972da
Berg Andrey <berg.andrey@icloud.com>
2023-12-19 15:35:21 +0300

    Minor Changes

--- public/admin/index.php
+++ public/admin/index.php
@@ -83,13 +83,14 @@ foreach ($fio as $value) {
         <div class="row">
             <div class="col-lg-12 row">
                 <h2>Поиск пользователей</h2>
-                <input type="text" name="search" id="search" autocomplete="off" placeholder="Поиск....">
+                <input type="text" class="form-control mb-2" name="search" id="search" autocomplete="off"
+                       placeholder="Поиск....">
             </div>
         </div>
         <!--TODO: перенести в блоки -->
         <div class="row">
             <div class="col-lg-12 row">
-                <button class="btn btn-primary btn-dark" type="button" data-bs-toggle="collapse"
+                <button class="btn btn-outline-primary active" type="button" data-bs-toggle="collapse"
                         data-bs-target="#ddd"
                         aria-expanded="false" aria-controls="collapseExample">
                     Расширенный поиск
@@ -99,35 +100,61 @@ foreach ($fio as $value) {
     </div>
     <div class="collapse" id="ddd">
         <div class="card card-body col-12 row">
+            <div class="row">
+                <div class="btn-group d-flex flex-column flex-md-row flex-lg-row" role="group"
+                     aria-label="Basic mixed styles example">
+                    <input type="radio" class="btn-check" name="btnradio" id="btnradio1"
+                           autocomplete="off" onchange="showMenu('personal_block')" checked>
+                    <label class="btn btn-outline-primary" for="btnradio1">Личные данные</label>
+
+                    <input type="radio" class="btn-check" name="btnradio" id="btnradio2"
+                           autocomplete="off" onchange="showMenu('workork_block')">
+                    <label class="btn btn-outline-primary" for="btnradio2">Работа</label>
+
+                    <input type="radio" class="btn-check" name="btnradio" id="btnradio3"
+                           autocomplete="off" onchange="showMenu('skills_block')">
+                    <label class="btn btn-outline-primary" for="btnradio3">Качества</label>
+                </div>
+            </div>
             <div id="personal_block">
                 <!--Upper-->
-                <div class="row">
+                <div class="row mt-2">
                     <!--Left-->
                     <div class="col">
                         <label class="form-label">Местоположение</label>
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" value="" id="address">
-                            <label class="form-check-label" for="address">
-                                Адрес
-                            </label>
+                            <label class="form-check-label" for="address" data-bs-toggle="collapse"
+                                   data-bs-target="#XYI">Адрес</label>
+                            <div class="collapse" id="XYI">
+                                <label><input type="text" class="form-control"></label>
+                            </div>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" value="" id="citizenship">
-                            <label class="form-check-label" for="citizenship">
-                                Гражданство
-                            </label>
+                            <label class="form-check-label" for="citizenship" data-bs-toggle="collapse"
+                                   data-bs-target="#XYI">Гражданство</label>
+                            <div class="collapse" id="XYI">
+                                <label><input type="text" class="form-control"></label>
+                            </div>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" value="" id="accommodations">
-                            <label class="form-check-label" for="accommodations">
-                                Условия проживания
-                            </label>
+                            <label class="form-check-label" for="accommodations" data-bs-toggle="collapse"
+                                   data-bs-target="#XYI">Условия проживания</label>
+                            <div class="collapse" id="XYI">
+                                <label><input type="text" class="form-control"></label>
+                            </div>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" value="" id="birthplace">
-                            <label class="form-check-label" for="birthplace">
+                            <label class="form-check-label" for="birthplace" data-bs-toggle="collapse"
+                                   data-bs-target="#XYI">
                                 Место рождения
                             </label>
+                            <div class="collapse" id="XYI">
+                                <label><input type="text" class="form-control"></label>
+                            </div>
                         </div>
                     </div>
                     <!--Right-->
@@ -135,16 +162,23 @@ foreach ($fio as $value) {
                         <label class="form-label">Личные данные</label>
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" value="" id="phone">
-                            <label class="form-check-label" for="phone">
+                            <label class="form-check-label" for="phone" data-bs-toggle="collapse" data-bs-target="#XYI">
                                 Мобильный телефон
                             </label>
+                            <div class="collapse" id="XYI">
+                                <label><input type="text" class="form-control"></label>
+                            </div>
                         </div>
 
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" value="" id="birthdate">
-                            <label class="form-check-label" for="birthdate">
+                            <label class="form-check-label" for="birthdate" data-bs-toggle="collapse"
+                                   data-bs-target="#XYI">
                                 Дата рождения
                             </label>
+                            <div class="collapse" id="XYI">
+                                <label><input type="text" class="form-control"></label>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -155,21 +189,31 @@ foreach ($fio as $value) {
                         <label class="form-label">Состав семьи</label>
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" value="" id="child">
-                            <label class="form-check-label" for="child">
+                            <label class="form-check-label" for="child" data-bs-toggle="collapse" data-bs-target="#XYI">
                                 Дети
                             </label>
+                            <div class="collapse" id="XYI">
+                                <label><input type="text" class="form-control"></label>
+                            </div>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" value="" id="spouse">
-                            <label class="form-check-label" for="spouse">
+                            <label class="form-check-label" for="spouse" data-bs-toggle="collapse"
+                                   data-bs-target="#XYI">
                                 Супруг(а)
                             </label>
+                            <div class="collapse" id="XYI">
+                                <label><input type="text" class="form-control"></label>
+                            </div>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" value="" id="solo">
-                            <label class="form-check-label" for="solo">
+                            <label class="form-check-label" for="solo" data-bs-toggle="collapse" data-bs-target="#XYI">
                                 Один
                             </label>
+                            <div class="collapse" id="XYI">
+                                <label><input type="text" class="form-control"></label>
+                            </div>
                         </div>
                     </div>
                     <!--Right-->
@@ -177,16 +221,29 @@ foreach ($fio as $value) {
                         <label class="form-label ">Образование</label>
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" value="" id="education_type">
-                            <label class="form-check-label" for="education_type">
+                            <label class="form-check-label" for="education_type" data-bs-toggle="collapse"
+                                   data-bs-target="#XYI">
                                 Образование
                             </label>
+                            <div class="collapse" id="XYI">
+                                <label><input type="text" class="form-control"></label>
+                            </div>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" value="" id="education">
-                            <label class="form-check-label" for="education">
+                            <label class="form-check-label" for="education" data-bs-toggle="collapse"
+                                   data-bs-target="#XYI">
                                 Окончил
                             </label>
+                            <div class="collapse" id="XYI">
+                                <label><input type="text" class="form-control"></label>
+                            </div>
                         </div>
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-lg-12 row">
+                        <button class="btn btn-primary">Прмиенить</button>
                     </div>
                 </div>
             </div>
@@ -290,21 +347,7 @@ foreach ($fio as $value) {
                 </div>
             </div>
             <!--Bottom-->
-            <div class="row">
-                <div class="btn-group d-flex flex-column flex-md-row flex-lg-row" role="group" aria-label="Basic mixed styles example">
-                    <input type="radio" class="btn-check" name="btnradio" id="btnradio1"
-                           autocomplete="off" onchange="showMenu('personal_block')" checked>
-                    <label class="btn btn-outline-dark btn-outline-primary" for="btnradio1">Личные данные</label>
 
-                    <input type="radio" class="btn-check" name="btnradio" id="btnradio2"
-                           autocomplete="off" onchange="showMenu('workork_block')">
-                    <label class="btn btn-outline-dark btn-outline-primary" for="btnradio2">Работа</label>
-
-                    <input type="radio" class="btn-check" name="btnradio" id="btnradio3"
-                           autocomplete="off" onchange="showMenu('skills_block')">
-                    <label class="btn btn-outline-dark btn-outline-primary" for="btnradio3">Качества</label>
-                </div>
-            </div>
         </div>
     </div>
 
